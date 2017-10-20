dd8ee1a7471e4d1155b7bcf27014dc7159b0a52c
Han You <youhan26@gmail.com>
2017-10-20 16:23:47 +0800

    init

---
+++ README.md
@@ -0,0 +1,29 @@
+
+# react-native-mob-sms
+
+## Getting started
+
+`$ npm install react-native-mob-sms --save`
+
+### Mostly automatic installation
+
+`$ react-native link react-native-mob-sms`
+
+### Manual installation
+
+
+#### iOS
+
+1. In XCode, in the project navigator, right click `Libraries` ➜ `Add Files to [your project's name]`
+2. Go to `node_modules` ➜ `react-native-mob-sms` and add `RNMobSms.xcodeproj`
+3. In XCode, in the project navigator, select your project. Add `libRNMobSms.a` to your project's `Build Phases` ➜ `Link Binary With Libraries`
+4. Run your project (`Cmd+R`)<
+
+## Usage
+```javascript
+import RNMobSms from 'react-native-mob-sms';
+
+// TODO: What to do with the module?
+RNMobSms;
+```
+  
---
+++ index.js
@@ -0,0 +1,33 @@
+/**
+ * Created by youhan on 2017/1/11.
+ */
+import {NativeModules} from 'react-native';
+
+const {MobSMS} = NativeModules;
+
+const registerApp = (appKey, appSecret) => {
+  MobSMS.registerApp(appKey, appSecret);
+};
+
+registerApp();
+
+const isError = (str) => {
+  return str.indexOf('null') === -1;
+};
+
+const getVerificationCode = (phoneNumber, zone, cb) => {
+  MobSMS.getVerificationCodeByMethod(0, phoneNumber, zone, null, (error) => {
+    cb(isError(error.code));
+  });
+};
+
+const commitVerificationCode = (code, phoneNumber, zone, cb) => {
+  MobSMS.commitVerificationCode(code, phoneNumber, zone, (error) => {
+    cb(isError(error.code));
+  });
+};
+
+export default {
+  getVerificationCode,
+  commitVerificationCode
+};
